f0d063eade44065178ba90e29d2052b851be3bdd
scottug256 <153395735+scottug256@users.noreply.github.com>
2024-02-20 19:50:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -14,9 +14,9 @@ global.owner = [
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['917605902011','256707521863'] 
+global.prems = ['917605902011', '32460248586', '256707521863']
+global.allowed = ['917605902011', '32460248586', '256707521863']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -52,7 +52,7 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = 'MORICEUG'
 global.premium = 'true'
-global.packname = '@jumota256' 
+global.packname = 'MORICE DESIGNZ' 
 global.author = 'MORICE' 
 global.menuvid = 'https://telegra.ph/file/085c4b1068f0f4f8db970.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
